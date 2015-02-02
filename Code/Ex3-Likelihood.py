e4af939f857a594d810f6e89e5310ad3ffe1c3f6
Zach Rodriguez <zrodri3@tigers.lsu.edu>
2015-02-01 18:37:31 -0600

    updated ex3

--- Code/Ex3-Likelihood.py
+++ Code/Ex3-Likelihood.py
@@ -6,32 +6,104 @@ Zachary Rodriguez
 """
 
 #My Imports
+import scipy
 from scipy.stats import binom
+from numpy import random
+import math
 
+"""
+This function takes the number of trials as an argument (n),
+assigns a random probability, then draws from a binomial distribution based on (n,p) 
+then queries the user to estimate the prob, based on # of successes (k)
+"""
 
-#1
+def in_class(n=5,p=0.5):
+	p = random.random()	#gives (p) a random value between 0 & 1
+	k = binom.rvs(n,p)	#draws from a binomial distribution based on (n,p), returns # of successes
+	print ("For (",n,") trials, the number of successes was:",k)
+	guess = float(input("What do you think the probability was?"))	#queries the user for a guess
+	if guess > 1:
+		guess /= 100
+	else:
+		guess = guess
+	error = 100*(abs(p-guess)/p)
+	if error < 5:
+		print ("Sweet! The probability was:",p,"\nYou're %error was:",error,"\nGo Buy a lotto ticket!")
+	elif error < 20:
+		print ("So Close! The probability was:",p,"\nYou're %error was:",error,"\nYou should try again.")
+	else:
+		print ("womp womp... The probability was:",p,"\nYou're %error was:",error,"\nBetter luck next time!")
+
+#This function sets up a list of probabilities from 0 -> 1 over a defined interval
+def probs(step=0.05):
+	p_list = []
+	p = 0
+	while p <= 1.001:		#range function doesn't take floats
+		p_list.append(p)
+		p += step
+	#print("Here is your list of probabilities:",[print (p) for p in p_list])
+	return (p_list)
+		
+"""
+#a calculation necessary for the binomial coefficient
+def multiply(min=1, max=10):
+	total = 1
+	for num in range(max,min-1,-1):	#iterates through range: max -> min, stepping -1 each time
+		total *= num
+	return (total)
+"""
+
 #Calculates the Binomial Coefficient (n choose k)
-def bcoef(n,k):
-	bcf = multiply((n-k+1),n)/multiply(max=k)
+def bincoef(n,k):
+	total = 1
+	for num in range(n,(n-k),-1):	#iterates through range
+		total *= num 
+	bcf = total/math.factorial(k)
+	#print ("Your binomial coefficient is:",bcf,"given (",n,"choose",k,")")
 	return bcf
 
 #Calculates the Binomial PMF
 def bpmf(n,k,p):
-	bcf = bcoef(n,k)
-	pmf = bcf1*pow(p,k)*pow(1-p,n-k)
-	return (pmf)
-	
-#This function sets up list of probabilities from 0 ->1, over a defined interval:
-def probs(step=0.05)
-	p_list = []
-	for p in range(0,1,step):
-		p_list.append(p)
-	print ("Here is your list of probabilities:",p_list,"\n")	#This is a check
-	return p_list
+	bc = bincoef(n,k)
+	pmf = bc*pow(p,k)*pow(1-p,n-k)
+	#print ("Your binomial pmf is:",pmf,"given",n,"trials,",k,"successes and a probability of:",p)
+	return pmf
 
-probs()	#Executes list
+#Calculates likelihood scores
+def likelihood(n,k,p):
+	pmf = bpmf(n,k,p)
+	bc = bincoef(n,k)
+	l = pmf/bc
+	#print ("Your Likelihood score for p =",p,"is:",l,",given",n,"trials and",k,"successes")
+	return l
 
-n = 5
-p = 0.5 # Change this and repeat
+#calculates maximum likelihood
+def ml(n,k):
+	lscores,lratios = [],[]
+	p_list = probs()
+	[lscores.append(likelihood(n,k,p)) for p in p_list]	#calculates likelihood score for each prob
+	maxl = max(lscores)
+	lindex = lscores.index(maxl)
+	maxp = p_list[lindex] 
+	for l in lscores:
+		ratio = abs(l-maxl)/maxl
+		lratios.append(ratio)
+	print ("Your ML score for p =",maxp,",is:",maxl,",given (",n,") trials and (",k,") successes")
+	return maxl,lratios
 
-data = binom.rvs(n,p)
+#These Are Tests----------------
+#in_class(100)
+#probs()
+#bpmf(5,1,.5)
+#likelihood(5,1,.5)
+#ml(100,20)
+
+
+
+
+
+
+
+
+
+
